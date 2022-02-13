68c8898808b1dedc42d92f9c519a3de6a1e652fb
Mert KAYATAY <96866157+MertKayatay@users.noreply.github.com>
2022-02-13 14:58:41 +0300

    Homework 4
    Homework 4

---
+++ Homework 4.sql
@@ -0,0 +1,12 @@
+1) SELECT DISTINCT replacement_cost FROM film
+
+2) SELECT COUNT(DISTINCT replacement_cost) FROM film
+
+3) SELECT COUNT(title) FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+4) SELECT COUNT(country) FROM country
+WHERE country LIKE '_____';
+
+5) SELECT COUNT(city) FROM city
+WHERE city ILIKE '%r';
