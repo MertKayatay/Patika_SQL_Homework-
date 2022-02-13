4935c76dd4156096e92699eab2ef42b72d3f3587
Mert KAYATAY <96866157+MertKayatay@users.noreply.github.com>
2022-02-13 13:29:04 +0300

    Homework 2
    SQL Homework 2

---
+++ Homework 2.sql
@@ -0,0 +1,12 @@
+1) SELECT * FROM film
+WHERE replacement_cost >= 12.99 AND replacement_cost < 16.99
+
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.98
+
+2) SELECT first_name, last_name FROM actor
+WHERE first_name IN('Penelope','Nick','Ed');
+
+3) SELECT * FROM film
+WHERE rental_rate IN(0.99, 2.99, 4.99) AND replacement_cost IN(12.99, 15.99, 28.99);
+
